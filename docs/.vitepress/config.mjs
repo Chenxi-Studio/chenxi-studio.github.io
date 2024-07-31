fe758962407e1d00b87439c15781843b25c07bdc
EthanYLin <yichenglin20@fudan.edu.cn>
2024-08-01 01:28:55 +0800

    initial commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "晨曦工作室",
+  description: "Chenxi Studio's homepage",
+
+  themeConfig: {
+
+    logo: '/logo.png',
+
+    outline: {
+      label: '目录'
+    },
+
+    nav: [
+      { text: '项目', link: '/pages/projects' },
+      { text: '动态', link: '/pages/latest' },
+      { text: '公告', link: '/pages/bulletin' },
+      { text: '关于', link: '/pages/about' },
+      { text: 'GitHub', link: 'https://github.com/vuejs/vitepress'},
+      { text: '友情链接', 
+        items: [
+          { text: 'FDCS', link: 'https://cs.fudan.edu.cn' },
+        ]
+      }
+    ],
+
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "晨曦工作室"
+  
+  tagline: Chenxi Studio
+  image:
+    src: /logo.png
+    alt: Chenxi Studio
+  actions:
+    - theme: brand
+      text: 项目
+      link: /pages/projects
+    - theme: alt
+      text: 动态
+      link: /pages/latest
+
+features:
+  - title: 🧑‍⚕️申爱 - SHAI
+    details: 血脉相通，献爱申城，用爱点亮祖国地图，做有情怀的献血志愿服务小程序…
+  - title: 🦁计遇 - JIYU
+    details: 计遇，开启你在FDUCS的无限可能，为计算机学院本研学生打造的活动一体化集中平台…
+  - title: 📰百事通 - inFDU
+    details: 汇聚复旦大学百家公众号信息，网罗校园最新资讯，打造最接地气的复旦热力榜…
+---
+
